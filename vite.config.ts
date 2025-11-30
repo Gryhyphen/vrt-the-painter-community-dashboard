214f4fdcfb96952c5668877e0fe6ce3d81d937a1
Gry- <gryhyphen@ninoa.lan>
2025-11-30 17:26:31 +1000

    Fix map contribute tool entry point

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/vrt-the-painter-community-dashboard/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        // main entry
+        main: resolve(__dirname, 'index.html'),
+        // secondary entry
+        mapcontribute: resolve(__dirname, 'tools/mapcontribute/index.html'),
+      },
+    },
+  },
+});
