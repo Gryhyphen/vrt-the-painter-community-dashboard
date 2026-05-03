92ff38ce39f94d01673081f9bb51c6807022b733
Gry- <gryhyphen@ninoa.lan>
2026-05-03 18:59:50 +1000

    Update youtube data
    Adds the new community video recorded by angel to the yt data section.

--- src/assets/youtubeData/youtubeData.ts
+++ src/assets/youtubeData/youtubeData.ts
@@ -1,4 +1,5 @@
 const videoUrls = [
+  "https://www.youtube.com/watch?v=_vLDmDHXK74",
   "https://www.youtube.com/watch?v=iU4Cskz6DnM",
   "https://www.youtube.com/watch?v=ZN4NWIU3sS4",
   "https://youtu.be/mMY_ifQlQZk?si=JYedkUoxV7rgPvYS",
